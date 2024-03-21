f281be9f7974c6161e3d631492191a8c98472a43
Edward Tadros <edward.tadros@proticom.com>
2024-03-20 21:38:34 -0700

    Fix signature verification in BlockchainController.js

--- BlockchainController.js
+++ BlockchainController.js
@@ -4,6 +4,9 @@
  * This class expose the endpoints that the client applications will use to interact with the 
  * Blockchain dataset
  */
+
+const logger = require('./src/logger.js');
+
 class BlockchainController {
 
     //The constructor receive the instance of the express.js app and the Blockchain class
@@ -54,25 +57,32 @@ class BlockchainController {
         });
     }
 
-    // Endpoint that allow Submit a Star, yu need first to `requestOwnership` to have the message (POST endpoint)
+    // Endpoint that allow Submit a Star, you need first to `requestOwnership` to have the message (POST endpoint)
     submitStar() {
         this.app.post("/submitstar", async (req, res) => {
+            logger.info('submitStar endpoint called');
             if(req.body.address && req.body.message && req.body.signature && req.body.star) {
                 const address = req.body.address;
                 const message = req.body.message;
                 const signature = req.body.signature;
                 const star = req.body.star;
+                logger.info(`Received data: address=${address}, message=${message}, signature=${signature}, star=${JSON.stringify(star)}`);
                 try {
+                    logger.info('Attempting to submit star');
                     let block = await this.blockchain.submitStar(address, message, signature, star);
                     if(block){
+                        logger.info('Star submitted successfully');
                         return res.status(200).json(block);
                     } else {
+                        logger.error('An error occurred while submitting star');
                         return res.status(500).send("An error happened!");
                     }
                 } catch (error) {
+                    logger.error('Exception caught in submitStar: ', error);
                     return res.status(500).send(error);
                 }
             } else {
+                logger.error('Invalid request body in submitStar');
                 return res.status(500).send("Check the Body Parameter!");
             }
         });
